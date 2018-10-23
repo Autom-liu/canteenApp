d6e4708ee427d4cd43f26a68a4e2c3870804e921
liusimin <liusimin@tungee.com>
2018-10-24 05:39:37 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+		<img src="@/assets/images/logo.jpg" alt="" width="100%" />
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+
+</style>
